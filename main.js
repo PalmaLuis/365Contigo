35eb41e31a442bc0d4ae04f8dad14d156632d07e
LuisPalma <luispalma1307@gmail.com>
2022-05-29 14:52:28 -0500

    se termino el proyecto pero falta dar retoques

---
+++ main.js
@@ -0,0 +1,42 @@
+alert(
+	'EN PRIMER LUGAR FELIZ CUMPLE WEEE, TE QUIERO MUUCHO SI PUEDO EL MIERCOLES LUEGO DE LA CHAMBA VOY A TU CASA CON UN KK DE 0.5 Y SU VELITA QUIERO FOTO CONTIGO AJAJA',
+);
+alert('LOS BOTONES BLANCOS SON TU RESPUESTA AMIX');
+
+const closecap7 = () => {
+	const capa7 = document.getElementById('capa7');
+	capa7.classList.add('hidden');
+};
+const closecap6 = () => {
+	const capa6 = document.getElementById('capa6');
+	capa6.classList.add('hidden');
+};
+const closecap5 = () => {
+	const capa5 = document.getElementById('capa5');
+	capa5.classList.add('hidden');
+};
+const closecap4 = () => {
+	const capa4 = document.getElementById('capa4');
+	capa4.classList.add('hidden');
+};
+const closecap3 = () => {
+  window.open("https://lenaycarbon.pe/ordena-aqui")
+	const capa3 = document.getElementById('capa3');
+	capa3.classList.add('hidden');
+};
+const closecap2 = () => {
+	const capa2 = document.getElementById('capa2_texto');
+	capa2.classList.add('hidden');
+};
+const closecap1 = () => {
+	const capa1 = document.getElementById('capa1_nelson');
+	capa1.classList.add('hidden');
+  alert("Yo tambien te quiero muchooooo wee :'c")
+  alert("Weeee perdon T.T de momento no cambia de frace porque no pude crearlas todas pero tenme pasiencia ya lo terminare T.T")
+};
+
+const bucle = () => {
+	const capa2 = document.getElementById('capa2_texto');
+	capa2.classList.remove('hidden');
+	alert('nene marcaste la opcion equivocada');
+};
